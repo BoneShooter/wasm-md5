5c30bcb96fc9d8a92beba1951ccdcb9dd466efb5
zhanghao <hzzhanghao2015@corp.netease.com>
2019-06-23 18:25:41 +0800

    add ALLOW_MEMORY_GROWTH=1 when build

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@ if [ ! -d "/build" ]; then
   mkdir /build
 fi
 cd src
+#  -s "ALLOW_MEMORY_GROWTH=1"
+#  -s "TOTAL_MEMORY=5265552"
 emcc main.cpp md5.cpp -s "ALLOW_MEMORY_GROWTH=1" -s "EXPORTED_FUNCTIONS=['_getMD5']" -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']" -o ../build/main.js
 cd ..
--- my-test-c/some-questions-about-c.md
+++ my-test-c/some-questions-about-c.md
@@ -11,3 +11,8 @@ char *md5
 // out = md5;
 // 有啥区别？？为什么=不行
 strncpy(out, md5, 128);
+
+
+### emcc 最大分配内存
+
+Uncaught abort("Stack overflow! Attempted to allocate 40000001 bytes on the stack, but stack has only 5242865 bytes available!") at Error
